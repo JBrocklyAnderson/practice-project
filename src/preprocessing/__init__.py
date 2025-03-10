9aadb501707f7cff401a26b23545655e3d9c0194
JBrocklyAnderson <140027721+JBrocklyAnderson@users.noreply.github.com>
2025-03-09 22:23:58 -0700

    Add import statement for ICS preprocessing

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -3,6 +3,7 @@ from .epss_preprocessing import run_epss_preprocessing
 from .kev_preprocessing import run_kev_preprocessing
 from .poc_preprocessing import run_poc_preprocessing
 from .nvd_preprocessing import run_nvd_preprocessing
+from .ics_preprocessing import run_ics_preprocessing
 from .cwe import (
     run_cwe_preprocessing,
     run_related_cwe_preprocessing,
